59f5cbee556f83b4a27e15d0ff5b27c28765bc2b
asant <asant@DESKTOP-UFEG5G5>
2018-03-05 19:41:59 -0500

    Commit buscacorazones 3

--- Buscaminas/src/buscaminas/Sembrador.java
+++ Buscaminas/src/buscaminas/Sembrador.java
@@ -1,21 +1,29 @@
 
 package buscaminas;
 
+import java.util.Random;
+
 public class Sembrador {
     
     void sembrarCorazones(Celda lasCeldas[][],int cantidad){
         
-        //for each en Java
-        for(Celda[] laFila:lasCeldas){
-            
-            for(Celda laCelda:laFila){
-                
+        Random aleatorio=new Random(System.currentTimeMillis());
+        int coraAleatorio,sumatoria=0;
+ 
+        for(Celda[] laFila:lasCeldas)
+        {
+            for(Celda laCelda:laFila)
+            {
+                coraAleatorio=aleatorio.nextInt(10);
+                if(coraAleatorio>5 && sumatoria<cantidad){
                 Corazon unCorazon=new Corazon();
                 laCelda.setMiCorazon(unCorazon);
-                
+                sumatoria++;
+                }
             }
-        }       
-    }  
+        }
+    }
 }
+
 
 
